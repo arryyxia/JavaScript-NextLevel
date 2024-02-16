036fe51d0c42eb823fddc8c1474d0c3cf090333d
Mahatma Arrayyan <xxryaan3010@gmail.com>
2024-02-16 18:00:28 +0800

    execution theory done

---
+++ Closure/main.js
@@ -0,0 +1,76 @@
+// 2. execution context, hoisting, & scope
+
+// console.log(nama);
+// var nama = "ryan";
+
+// creation phase in global context
+// nama var = undifined
+// nama function = fn()
+// hoisting ^
+//          |
+
+// window = global object
+// this = window
+// kedua ini pasti dibuat ketika javascript dijalankan
+
+// execution phase top to bottom
+// console.log(hbd())
+
+// var umur = 30;
+
+// function hbd(){
+//     return `halo ${nama}! selamat ulangtahun yang ke ${umur}!`
+// }
+// function membuat local execution context
+// yang di dalamanya terdapat creation dan execution phase
+// dapat mengakses window dan "arguments" yang berisi apapun yang kita masukkan sebagai parameter
+// hoisting
+
+var nama = "ryan";
+var username = "@mht.aryn"
+
+function cetakUrl(username){
+    var urlIG = `https://instagram.com/profile/${username}`
+    console.log(arguments[0])
+
+    return urlIG
+}
+
+console.log(cetakUrl("@arryyxia"))
+
+// function a(){
+//     console.log("ini adalah function a")
+//     function b(){
+//         console.log("ini adalah function b")
+//         function c(){
+//             console.log("ini adalah function c")
+//         }
+//         c()
+//     }
+//     b()
+// }
+// a()
+// tumpukan exekusi
+// satu satu di eksekusi lalu di hilangkan setelah ter eksekusi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
